0e17a2fea5b70719d144f16f7aaa61b272993401
Bruce Ruffin <bruffin1221@gmail.com>
2019-03-29 17:34:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
   if false
-      puts (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      returns (board[index] == " ") || (board[index] == "") || (board[index] == nil)
   else true
-    puts (board[index] == "X") || (board[index] == "O")
+    returns (board[index] == "X") || (board[index] == "O")
   end
 end
